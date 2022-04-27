010fefa01009bd97eed03e84fd15db94d9ed9ed7
loremedda <lorenzo.medda.sa@nttdata.com>
2022-04-27 11:58:37 +0200

    add spacing to rows in data grid

--- src/pages/dashboardGroups/components/GroupsTableProduct/components/GroupsProductTable.tsx
+++ src/pages/dashboardGroups/components/GroupsTableProduct/components/GroupsProductTable.tsx
@@ -1,7 +1,13 @@
 import { ArrowDropDown, ArrowDropUp } from '@mui/icons-material';
 import { Box, styled } from '@mui/material';
-import { DataGrid, GridColDef, GridSortDirection, GridSortModel } from '@mui/x-data-grid';
-import React, { useMemo } from 'react';
+import {
+  DataGrid,
+  GridColDef,
+  GridRowSpacingParams,
+  GridSortDirection,
+  GridSortModel,
+} from '@mui/x-data-grid';
+import React, { useCallback, useMemo } from 'react';
 import { CustomPagination } from '@pagopa/selfcare-common-frontend';
 import { Page } from '@pagopa/selfcare-common-frontend/model/Page';
 import { useTranslation } from 'react-i18next';
@@ -116,6 +122,14 @@ export default function GroupsProductTable({
     [party, product, groups]
   );
 
+  const getRowSpacing = useCallback(
+    (params: GridRowSpacingParams) => ({
+      top: params.isFirstVisible ? 0 : 6,
+      bottom: params.isLastVisible ? 0 : 6,
+    }),
+    []
+  );
+
   return (
     <React.Fragment>
       <Box
@@ -129,6 +143,7 @@ export default function GroupsProductTable({
       >
         <CustomDataGrid
           className="CustomDataGrid"
+          getRowSpacing={getRowSpacing}
           autoHeight={true}
           rows={groups}
           rowCount={Math.max(page?.totalElements ?? 0, groups.length)}
