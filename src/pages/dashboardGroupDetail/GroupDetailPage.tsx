9d1f72a2275949b827551483c69903d10fec0fca
USERSAD\esignore <elena.signore@nttdata.com>
2022-05-26 18:49:40 +0200

    SELC-1122_modified group detail

--- src/pages/dashboardGroupDetail/GroupDetailPage.tsx
+++ src/pages/dashboardGroupDetail/GroupDetailPage.tsx
@@ -7,6 +7,8 @@ import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
 import { userSelectors } from '@pagopa/selfcare-common-frontend/redux/slices/userSlice';
 import { User } from '@pagopa/selfcare-common-frontend/model/User';
+import SupervisedUserCircle from '@mui/icons-material/SupervisedUserCircle';
+import ArrowBack from '@mui/icons-material/ArrowBack';
 import ProductNavigationBar from '../../components/ProductNavigationBar';
 import withGroupDetail, { withGroupDetailProps } from '../../decorators/withGroupDetail';
 import { DASHBOARD_GROUPS_ROUTES } from '../../routes';
@@ -63,7 +65,7 @@ function GroupDetailPage({ partyGroup, party, productsMap, productsRolesMap }: P
       onClick: goBack,
     },
     {
-      description: `${partyGroupState.name}`,
+      description: 'Anagrafe',
     },
   ];
 
@@ -84,8 +86,29 @@ function GroupDetailPage({ partyGroup, party, productsMap, productsRolesMap }: P
       mt={10}
       sx={{ width: '985px', backgroundColor: 'transparent !important' }}
     >
-      <Grid item xs={12} mb={3}>
-        <ProductNavigationBar paths={paths} />
+      <Grid item xs={12} mb={3} display="flex">
+        <Box mr={3}>
+          <Link
+            sx={{
+              cursor: 'pointer',
+              ml: 1,
+              textDecoration: 'none',
+              fontWeight: 'bold',
+              fontSize: '18px',
+              display: 'flex',
+            }}
+            onClick={goBack}
+          >
+            <ArrowBack color="primary" fontSize="small" sx={{ marginTop: '3px', mr: 1 }} />
+            {t('groupDetailPage.backActionLabel')}
+          </Link>
+        </Box>
+        <Box mr={2}>
+          <SupervisedUserCircle />
+        </Box>
+        <Box>
+          <ProductNavigationBar paths={paths} />
+        </Box>
       </Grid>
       <Grid container item mb={3}>
         <Grid item xs={6}>
@@ -94,7 +117,7 @@ function GroupDetailPage({ partyGroup, party, productsMap, productsRolesMap }: P
               <Typography variant="h1">{t('groupDetailPage.title')}</Typography>
             </Box>
             <Box>
-              {isSuspended && (
+              {!isSuspended && (
                 <Chip
                   label={t('groupDetail.status')}
                   aria-label="Suspended"
@@ -114,17 +137,6 @@ function GroupDetailPage({ partyGroup, party, productsMap, productsRolesMap }: P
               )}
             </Box>
           </Box>
-        </Grid>
-        <Grid item xs={6} display="flex" alignItems="center" justifyContent="flex-end">
-          <Link
-            sx={{
-              fontWeight: 600,
-              cursor: 'pointer',
-            }}
-            onClick={goBack}
-          >
-            {t('groupDetailPage.backActionLabel')}
-          </Link>
         </Grid>
       </Grid>
       <Grid container item xs={12} sx={{ backgroundColor: '#FFFFFF', padding: '24px' }}>
